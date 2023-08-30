45f7b758fc5e7bb3a283224d8f4dc5c309c854db
fanetech <louguefranck.20@gmail.com>
2023-08-30 19:04:33 +0000

    elevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,8 @@ class soir extends StatefulWidget {
 
 class _soirState extends State<soir> {
   var info = "Hello World";
+  var text1 = "test1";
+  var text2 = "test2";
 
   @override
   void initState() {
@@ -34,19 +36,46 @@ class _soirState extends State<soir> {
     info = "Bienvenue";
   }
 
+  void click1() {
+    setState(() {
+      text1 = "click text1";
+    });
+  }
+
+  void click2(String t) {
+    setState(() {
+      text2 = t;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(info)),
-      body: ElevatedButton(
-        child: const Text("Click Me"),
-        onPressed: () {
-          print("Hello World");
-          setState(() {
-            info = "Fanetech";
-          });
-        },
-      ),
-    );
+        appBar: AppBar(title: Text(info)),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () => click1(),
+                child: const Text('btn1',
+                    style: TextStyle(fontSize: 20, color: Colors.white))),
+            Text(
+              text1,
+              style: const TextStyle(color: Colors.red),
+            ),
+            const Divider(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () => click2("click text2"),
+                child: const Text('btn2',
+                    style: TextStyle(fontSize: 20, color: Colors.green))),
+            Text(
+              text2,
+              style: const TextStyle(color: Colors.green),
+            ),
+          ],
+        ));
   }
 }
